423415e4b309f8fe02943f4cedf695c62cae1c73
Edward <ebarrero0@gmail.com>
2022-03-25 20:06:20 -0500

    first commit

---
+++ Blog/Blog/urls.py
@@ -0,0 +1,17 @@
+
+from django.contrib import admin
+from django.urls import path
+from .import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('about/', views.about),  
+    path( 'home/', views.homepage),
+    path('menu/', views.menu),
+    path('mensajes/',views.mensajes),
+    path('musica-ingles/',views.ingles),
+    path('espa/',views.espa), 
+
+
+]
---
+++ Blog/Blog/views.py
@@ -0,0 +1,30 @@
+from urllib import request
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+
+
+def homepage(request):
+    #return HttpResponse('homepage')
+    return render(request,"homepage.html")
+
+
+def about(request):
+    #return HttpResponse('about')
+    return render(request,"about.html")
+
+
+def menu(request):
+    return HttpResponse('menu')
+
+def mensajes(request):
+    return HttpResponse('menajes')
+
+def ingles(request):
+    #return HttpResponse('ingles')
+    return render(request,"music_ing.html")
+def espa(request):
+    #return HttpResponse('espa')
+    return render(request,"espa.html")
